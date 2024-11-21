c25f2f26589759b4c88d66b1e2f7dfdc1da7c1b0
Zlog <zlog@zlog.pl>
2024-11-21 18:52:17 +0100

    zlog: escape path that could be mistaken as argument in script

--- reload_python_app.sh
+++ reload_python_app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-SCRIPT_DIR="/$0"; SCRIPT_DIR="${SCRIPT_DIR%/*}"; SCRIPT_DIR="${SCRIPT_DIR:-.}"; SCRIPT_DIR="${SCRIPT_DIR##/}/"; SCRIPT_DIR="$(cd "$SCRIPT_DIR"; pwd)"
+SCRIPT_DIR="/$0"; SCRIPT_DIR="${SCRIPT_DIR%/*}"; SCRIPT_DIR="${SCRIPT_DIR:-.}"; SCRIPT_DIR="${SCRIPT_DIR##/}/"; SCRIPT_DIR="$(cd -- "$SCRIPT_DIR"; pwd)"
 
 docker compose -f "$SCRIPT_DIR/"docker-compose.yml exec python sh -c 'UWSGI_PID="$(ps | grep '\'' /opt/venv/bin/uwsgi '\'' | head -1 | sed '\''s/^ *\([^ ]*\) .*/\1/'\'')"; kill -1 "$UWSGI_PID"'
