434c1b162473fc42c654e0c24e915a9df38901cf
Zlog <zlog@zlog.pl>
2025-03-10 14:48:26 +0100

    in progress

--- src/models/utils/EnvConsts.py
+++ src/models/utils/EnvConsts.py
@@ -1,17 +1,19 @@
 import os
-from typing import Any, Callable
 from .Singleton import Singleton
+from typing import Any, Self, Callable
 
 class EnvConsts(metaclass=Singleton):
     def __init__(self):
-        self.__PEPPER__=self.__getenv_or_exception('PEPPER_VAL')
+        # self.__PEPPER__=self.__getenv_or_exception('PEPPER_VAL')
         for m_fun in self.__mandytory:
-            m_fun()
+            m_fun(self)
 
     def getFlaskKey(self) -> str:
         return self.__getenv_or_exception('FLASK_KEY')
     def getAppDir(self) -> str:
         return self.__getenv_or_exception('APP_DIR')
+    # def getServerName(self) -> str:
+    #     return self.__getenv_or_exception('SERVER_NAME')
     
     def getDBString(self) -> str:
         return f'postgresql://{self.getDBUser()}:{self.getDBPass()}@{self.getDBHost()}/{self.getDB()}'
@@ -39,10 +41,16 @@ class EnvConsts(metaclass=Singleton):
             ,self.__getenv_or_exception('MAIL_AUTH_TYPE')
         )
     
-    def getPepper(self) -> str:
-        return self.__PEPPER__
+    # def getPepper(self) -> str:
+    #     return self.__PEPPER__
     
-    __mandytory: set[Callable[[], Any]]={getFlaskKey, getAppDir, getDBString, getMailData}
+    __mandytory: set[Callable[[Self], Any]]={
+        getFlaskKey
+        # ,getServerName
+        ,getAppDir
+        ,getDBString
+        ,getMailData
+    }
     def __getenv_or_exception(self, name: str) -> str:
         ret=self.__getenv(name)
         if ret is None:
