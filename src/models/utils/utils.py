732c22eccbf2a50d2caef2c0ce95d44771123872
Zlog <zlog@zlog.pl>
2025-04-26 18:02:47 +0200

    poprawienie komunikatu

--- src/models/utils/utils.py
+++ src/models/utils/utils.py
@@ -1,11 +1,12 @@
 import sys
 import os.path
 import traceback
+import typing as t
 from . import consts as c
-from flask import render_template, g
 from datetime import datetime,timezone
 from .EnvConsts import envConsts as ec
 from models.lang import LangEnum, LangBaseEx
+from flask import render_template, g, url_for
 from .FormNotFilledException import FormNotFilledException
 from werkzeug.datastructures.structures import ImmutableMultiDict
 
@@ -26,8 +27,19 @@ def get_from_form(form: ImmutableMultiDict[str, str], keys: tuple[str, ...]) -> 
         vs.append(v)
     return tuple(vs)
 
+P=t.ParamSpec('P')
+
+def _url_with_lang_for(_: t.Callable[t.Concatenate[str, P], str]=url_for) -> t.Callable[t.Concatenate[str, P], str]:
+    def func(endpoint: str, *args: P.args, **kwargs: P.kwargs) -> str:
+        path=url_for(endpoint, *args, **kwargs)
+        return url_for('choose_lang', lang=LangEnum(get_lang_pkg()), path=path.removeprefix('/'))
+    return func
+url_with_lang_for=_url_with_lang_for()
+del _url_with_lang_for
+
 def render_base_template(name: str, **kwargs: object) -> str:
-    return render_template(name, url_last_edit=url_last_edit, **kwargs)
+    global url_with_lang_for
+    return render_template(name, url_last_edit=url_last_edit, url_with_lang_for=url_with_lang_for, lang_pkg=get_lang_pkg(), **kwargs)
 
 def set_lang_pkg(lang: LangEnum) -> None:
     g.lang=lang.value
